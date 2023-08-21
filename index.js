5faf6f40c8e629d90d2bcf448c7f947e32acf5b3
redlemon8 <hanquezbenjamin@gmx.com>
2023-08-21 22:20:36 +0200

    add JS movement

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const infoContainer = document.querySelector(".info-container");
-const showTitle = document.querySelector("h1");
 const mousemove = document.querySelector(".mousemove");
 
 
@@ -7,6 +5,39 @@ const mousemove = document.querySelector(".mousemove");
 window.addEventListener("mousemove", (e) => {
      mousemove.style.left = e.pageX + "px";
      mousemove.style.top = e.pageY + "px";
-     console.log(e);
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const title = document.getElementById('title');
+    title.style.opacity = '0';
+    title.style.transform = 'translateX(-150%)';
+
+    setTimeout(() => {
+        title.style.opacity = '1';
+        title.style.transform = 'translateX(0)';
+    }, 100);
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const move = document.getElementById('move');
+    move.style.opacity = '0';
+    move.style.transform = 'translateY(150%)';
+
+    setTimeout(() => {
+        move.style.opacity = '1';
+        move.style.transform = 'translateY(0)';
+    }, 100);
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const slide = document.getElementById('slide');
+    slide.style.opacity = '0';
+    slide.style.transform = 'translateX(150%)';
+
+    setTimeout(() => {
+        slide.style.opacity = '1';
+        slide.style.transform = 'translateX(0)';
+    }, 100);
+});
+
 
