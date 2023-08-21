c5bd793fc97a2a248382847c93dee61ea01cf96b
redlemon8 <hanquezbenjamin@gmx.com>
2023-08-21 22:40:37 +0200

    remove margin to the title

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const title = document.getElementById('title');
     title.style.opacity = '0';
-    title.style.transform = 'translateX(-150%)';
+    title.style.transform = 'translateX(-200%)';
 
     setTimeout(() => {
         title.style.opacity = '1';
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', () => {
 document.addEventListener('DOMContentLoaded', () => {
     const move = document.getElementById('move');
     move.style.opacity = '0';
-    move.style.transform = 'translateY(150%)';
+    move.style.transform = 'translateY(200%)';
 
     setTimeout(() => {
         move.style.opacity = '1';
@@ -24,7 +24,7 @@ document.addEventListener('DOMContentLoaded', () => {
 document.addEventListener('DOMContentLoaded', () => {
     const slide = document.getElementById('slide');
     slide.style.opacity = '0';
-    slide.style.transform = 'translateX(150%)';
+    slide.style.transform = 'translateX(200%)';
 
     setTimeout(() => {
         slide.style.opacity = '1';
